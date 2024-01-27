5a514c11329bd09f0116d7bfc2ba97710b6ffbc8
LongNQ <long.nq.hust@gmail.com>
2024-01-27 17:17:32 +0700

    1. init project and add libs - package morgan: for debugging request - package helmet: hide important info at response - package compression: compress data response =>  1.4MB  => 3.8kB

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require('morgan')
+const app = express()
+
+// init middleware
+app.use(morgan('combined'))
+app.use(helmet())
+app.use(compression())
+// init db
+
+// init  routes
+app.use('/', (req,res,next) => {
+    const strCompress = "hello world"
+    return res.status(500).json({
+        message:'Welcome',
+        data: strCompress.repeat(10000)
+    })
+})
+
+// handle error
+
+module.exports = app
